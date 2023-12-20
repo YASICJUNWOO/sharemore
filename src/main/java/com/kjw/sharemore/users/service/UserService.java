e14c1072989cfe151be582e12ac2a5236ef01984
YASICJUNWOO <joonoo3@naver.com>
2023-12-21 00:10:34 +0900

    chore : 메소드 설명 추가

--- src/main/java/com/kjw/sharemore/users/service/UserService.java
+++ src/main/java/com/kjw/sharemore/users/service/UserService.java
@@ -1,11 +1,9 @@
 package com.kjw.sharemore.users.service;
 
-import com.kjw.sharemore.apiPayLoad.exception.handler.CustomExceptionHandler;
-import com.kjw.sharemore.users.dto.UserDetailResponseDTO;
-import com.kjw.sharemore.users.entity.Users;
 import com.kjw.sharemore.users.converter.UserConverter;
+import com.kjw.sharemore.users.dto.UserDetailResponseDTO;
 import com.kjw.sharemore.users.dto.UserRequestDTO;
-import com.kjw.sharemore.users.dto.UserResponseDTO;
+import com.kjw.sharemore.users.entity.Users;
 import com.kjw.sharemore.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,27 +18,62 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    /**
+     * @param :
+     * @return :
+     * @methodName : getUserList
+     * @Description: 전체 유저 조회
+     * @note:
+     **/
     public List<UserDetailResponseDTO> getUserList() {
         return userRepository.findAll().stream().map(
                 UserConverter::toUserDetailResponseDTO
         ).toList();
     }
 
+    /**
+     * @param :
+     * @return :
+     * @methodName : addUser
+     * @Description: 회원 추가
+     * @note:
+     **/
     public UserDetailResponseDTO addUser(UserRequestDTO userRequestDTO) {
         Users users = UserConverter.toEntity(userRequestDTO);
         return UserConverter.toUserDetailResponseDTO(userRepository.save(users));
     }
 
+    /**
+     * @param :
+     * @return :
+     * @methodName : getUserByEmail
+     * @Description: email로 유저 조회
+     * @note:
+     **/
     public Users getUserByEmail(String email) {
-        return userRepository.findByEmail(email).orElseThrow(new CustomExceptionHandler());
+        return userRepository.findByEmail(email).orElseThrow();
     }
 
+    /**
+     * @param :
+     * @return :
+     * @methodName : updateUser
+     * @Description: 유저 정보 수정
+     * @note:
+     **/
     public UserDetailResponseDTO updateUser(String email, UserRequestDTO userRequestDTO) {
         Users user = userRepository.findByEmail(email).orElseThrow();
         user.update(userRequestDTO);
         return UserConverter.toUserDetailResponseDTO(userRepository.save(user));
     }
 
+    /**
+     * @param :
+     * @return :
+     * @methodName : deleteUser
+     * @Description: 유저 삭제
+     * @note:
+     **/
     public UserDetailResponseDTO deleteUser(String email) {
         Users user = userRepository.findByEmail(email).orElseThrow();
         userRepository.delete(user);
